01b552b9872aea3d40d022b963f882a5f276502a
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-07-12 11:20:57 -0400

    merge and quick

--- DataStructures/sorted_algorithms.py
+++ DataStructures/sorted_algorithms.py
@@ -1,5 +1,6 @@
 from typing import List
 from .value import Value
+import random
 
 
 def comp_desc(a: int | float | Value, b: int | float | Value) -> int | float | Value:
@@ -39,6 +40,80 @@ def insertion_sort(l: List[int] | List[float] | List[Value], asc=True):
 
     for i in range(len(l)):
         j = i
-        while j != 0 and comp(l[j], l[j-1]):
+        while j != 0 and comp(l[j-1], l[j]):
             l[j-1], l[j] = l[j], l[j-1]
             j -= 1
+
+
+def merge_sort(l: List[int] | List[float] | List[Value], asc=True):
+    comp = comp_desc if asc else comp_asc
+    sort(l, comp, 0, len(l)-1)
+
+
+def sort(l: List[int] | List[float] | List[Value], comp, l1, r1):
+    if l1 == r1:
+        return
+
+    sort(l, comp, l1, (l1+r1)//2)
+    sort(l, comp, (l1+r1)//2+1, r1)
+    merge(l, comp, l1, (l1+r1)//2, (l1+r1)//2+1, r1)
+
+
+def merge(l: List[int] | List[float] | List[Value], comp, l1, r1, l2, r2):
+    aux = l.copy()
+    i, j = l1, l2
+    x = min(l1, l2)
+
+    while i <= r1 and j <= r2:
+        if not comp(aux[i], aux[j]):
+            l[x] = aux[i]
+            i += 1
+        else:
+            l[x] = aux[j]
+            j += 1
+
+        x += 1
+
+    while i <= r1:
+        l[x] = aux[i]
+        i += 1
+        x += 1
+
+    while j <= r2:
+        l[x] = aux[j]
+        j += 1
+        x += 1
+
+
+def quick_sort(l: List[int] | List[float] | List[Value], asc=True):
+    comp = comp_desc if asc else comp_asc
+    sort(l, comp, 0, len(l)-1)
+
+
+def quick(l: List[int] | List[float] | List[Value], comp, l1, r1):
+    if l1 > r1:
+        return
+
+    q = random.randint(l1, r1)
+    aux = l.copy()
+
+    i, j = l1, l1
+
+    while i <= r1:
+        if not comp(aux[i], aux[q]) and i != q:
+            l[j] = aux[i]
+            j += 1
+        i += 1
+
+    l[j] = aux[q]
+    j += 1
+
+    i = l1
+    while i <= r1:
+        if comp(aux[i], aux[q]) and i != q:
+            l[j] = aux[i]
+            j += 1
+        i += 1
+
+    quick(l, comp, l1, q-1)
+    quick(l, comp, q+1, r1)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from DataStructures.disjoin_set import DisjoinSet
-from DataStructures.sorted_algorithms import selection_sort,insertion_sort
+from DataStructures.sorted_algorithms import selection_sort, insertion_sort, merge_sort, quick_sort
 
-l = [3, 2, 5, -1, 0]
-insertion_sort(l, False)
+l = [3, 0, 5, -1, 0, 11, 12]
+quick_sort(l, False)
 print(l)
