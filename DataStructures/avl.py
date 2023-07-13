e7ee98b26ae134123313eb258313f66bb1b52e33
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2023-07-13 02:27:41 -0400

    multi set

--- DataStructures/avl.py
+++ DataStructures/avl.py
@@ -2,16 +2,16 @@ from .value import Value
 from typing import Tuple, Iterable
 
 
-class Node:
+class Bst:
     def __init__(self, k: int | float | Value, v=None):
         self.key: int | float | Value = k
         self.value = v
         self.size: int = 1
         self.h = 1
-        self.left: Node = None
-        self.right: Node = None
+        self.left: Bst = None
+        self.right: Bst = None
 
-    def pre_orden(self) -> Iterable['Node']:
+    def pre_orden(self) -> Iterable['Bst']:
         yield self
 
         if self.left is not None:
@@ -21,7 +21,7 @@ class Node:
         for i in self.right.pre_orden():
             yield i
 
-    def in_orden(self) -> Iterable['Node']:
+    def in_orden(self) -> Iterable['Bst']:
         if self.left is not None:
             for i in self.left.in_orden():
                 yield i
@@ -32,7 +32,7 @@ class Node:
             for i in self.right.in_orden():
                 yield i
 
-    def post_orden(self) -> Iterable['Node']:
+    def post_orden(self) -> Iterable['Bst']:
         if self.left is not None:
             for i in self.left.post_orden():
                 yield i
@@ -43,7 +43,7 @@ class Node:
 
         yield self
 
-    def find(self, k: int | float | Value) -> 'Node':
+    def find(self, k: int | float | Value) -> 'Bst':
         if self.key == k:
             return self
 
@@ -51,6 +51,19 @@ class Node:
             return None if self.left is None else self.left.find(k)
 
         return None if self.right is None else self.right.find(k)
+
+    def update(self, k: int | float | Value):
+        if self.key == k:
+            return
+
+        if k < self.key and self.left is not None:
+            self.left.update(k)
+
+        if k > self.key and self.right is not None:
+            self.right.update(k)
+
+        self.act_h()
+        self.act_size()
 
     def rank(self, k: int | float | Value) -> int:
         left = 0 if self.left is None else self.left.size
@@ -63,7 +76,7 @@ class Node:
 
         return left + 1 if self.right is None else left + 1 + self.right.rank(k)
 
-    def select(self, index: int) -> 'Node':
+    def select(self, index: int) -> 'Bst':
         if index < 0 or index >= self.size:
             return None
 
@@ -90,103 +103,177 @@ class Node:
         self.size = sl + sr + 1
 
 
-class Avl:
-    def __init__(self):
-        self.root: Node = None
+class MultiBst(Bst):
+    def __init__(self, k: int | float | Value, v=None):
+        super().__init__(k, v)
 
-    def pre_orden(self) -> Iterable[Node]:
+    def pre_orden(self) -> Iterable['MultiBst']:
+        for _ in range(self.value):
+            yield self
+
+        if self.left is not None:
+            for i in self.left.pre_orden():
+                yield i
+
+        for i in self.right.pre_orden():
+            yield i
+
+    def in_orden(self) -> Iterable['MultiBst']:
+        if self.left is not None:
+            for i in self.left.in_orden():
+                yield i
+
+        for _ in range(self.value):
+            yield self
+
+        if self.right is not None:
+            for i in self.right.in_orden():
+                yield i
+
+    def post_orden(self) -> Iterable['MultiBst']:
+        if self.left is not None:
+            for i in self.left.post_orden():
+                yield i
+
+        if self.right is not None:
+            for i in self.right.post_orden():
+                yield i
+
+        for _ in range(self.value):
+            yield self
+
+    def rank(self, k: int | float | Value) -> int:
+        left = 0 if self.left is None else self.left.size
+
+        if k == self.key:
+            return left
+
+        if k < self.key:
+            return 0 if self.left is None else self.left.rank(k)
+
+        return left + self.value if self.right is None else left + self.value + self.right.rank(k)
+
+    def select(self, index: int) -> 'MultiBst':
+        if index < 0 or index >= self.size:
+            return None
+
+        left = 0 if self.left is None else self.left.size
+
+        if left <= index and index < left + self.value:
+            return self
+
+        if index < left:
+            return None if self.left is None else self.left.select(index)
+
+        return None if self.right is None else self.right.select(index - left - self.value)
+
+    def act_size(self):
+        sl: int = 0 if self.left is None else self.left.size
+        sr: int = 0 if self.right is None else self.right.size
+
+        self.size = sl + sr + self.value
+
+
+class Avl:
+    def __init__(self, init: callable = Bst):
+        self.root: Bst = None
+        self.init = init
+
+    def pre_orden(self) -> Iterable[Bst]:
         if self.root is None:
             return []
         return self.root.pre_orden()
 
-    def in_orden(self) -> Iterable[Node]:
+    def in_orden(self) -> Iterable[Bst]:
         if self.root is None:
             return []
         return self.root.in_orden()
 
-    def post_orden(self) -> Iterable[Node]:
+    def post_orden(self) -> Iterable[Bst]:
         if self.root is None:
             return []
         return self.root.post_orden()
 
-    def find(self, k: int | float | Value) -> Node:
+    def find(self, k: int | float | Value) -> Bst:
         if self.root is None:
             return None
         return self.root.find(k)
 
+    def update(self, k: int | float | Value):
+        if self.root is None:
+            return
+        self.root.update(k)
+
     def rank(self, k: int | float | Value) -> int:
         return self.root.rank(k)
 
-    def select(self, index: int) -> Node | None:
+    def select(self, index: int) -> Bst | None:
         if self.root is None:
             return None
         return self.root.select(index)
 
     def insert(self, k: int | float | Value, v=None):
         if self.root is None:
-            self.root = Node(k, v)
+            self.root = Bst(k, v)
             return
 
-        Avl.__insert(self.root, k, v)
+        self.__insert(self.root, k, v)
         self.__valance__root()
 
     def remove(self, k: int | float | Value):
         if self.root is not None and self.root.key == k:
-            self.root = Avl.__remove_node(self.root)
+            self.root = self.__remove_node(self.root)
         else:
-            Avl.__remove(self.root, k)
+            self.__remove(self.root, k)
 
         self.__valance__root()
 
-    @staticmethod
-    def __insert(node: Node, k: int | float | Value, v=None):
+    def __insert(self, node: Bst, k: int | float | Value, v=None):
         if node.key == k:
             return
 
         if k < node.key:
             if node.left is None:
-                node.left = Node(k, v)
+                node.left = self.init(k, v)
             else:
-                Avl.__insert(node.left, k, v)
+                self.__insert(node.left, k, v)
         else:
             if node.right is None:
-                node.right = Node(k, v)
+                node.right = self.init(k, v)
             else:
-                Avl.__insert(node.right, k, v)
+                self.__insert(node.right, k, v)
 
         node.act_h()
         node.act_size()
 
         Avl.__valance(node)
 
-    @staticmethod
-    def __remove(node: Node | None, k: int | float | Value):
+    def __remove(self, node: Bst | None, k: int | float | Value):
         if node is None:
             return
 
         find = False
 
         if node.left is not None and node.left.key == k:
-            node.left = Avl.__remove_node(node.left)
+            node.left = self.__remove_node(node.left)
             find = True
 
         if node.right is not None and node.right.key == k:
-            node.right = Avl.__remove_node(node.right)
+            node.right = self.__remove_node(node.right)
             find = True
 
         if not find:
             if node.key < k:
-                Avl.__remove(node.left, k)
+                self._remove(node.left, k)
             else:
-                Avl.__remove(node.right, k)
+                self._remove(node.right, k)
 
         node.act_h()
         node.act_size()
 
         Avl.__valance(node)
 
-    @staticmethod
-    def __remove_node(node: Node) -> Node:
+    def __remove_node(self, node: Bst) -> Bst:
         if node.left is None and node.right is None:
             return None
 
@@ -198,17 +285,17 @@ class Avl:
             return node.left
 
         k = node.key
-        aux = Avl.__select(node.right, 0)
+        aux = node.right.select(0)
 
         node.key, node.value = aux.key, aux.value
         aux.key = k
 
-        Avl.__remove(node, k)
+        self.__remove(node, k)
 
         return node
 
     @staticmethod
-    def __f_valance(node: Node | None) -> int:
+    def __f_valance(node: Bst | None) -> int:
         if node is None:
             return 0
 
@@ -224,7 +311,7 @@ class Avl:
             self.root = Avl.__valance_l(self.root)
 
     @staticmethod
-    def __valance(node: Node):
+    def __valance(node: Bst):
         if Avl.__f_valance(node.left) == 2:
             node.left = Avl.__valance_r(node.left)
         if Avl.__f_valance(node.left) == -2:
@@ -236,21 +323,21 @@ class Avl:
             node.right = Avl.__valance_l(node.right)
 
     @staticmethod
-    def __valance_r(node: Node) -> Node:
+    def __valance_r(node: Bst) -> Bst:
         if Avl.__f_valance(node.right) == -1:
             node.right = Avl.__valance_l_s(node.right)
 
         return Avl.__valance_r_s(node)
 
     @staticmethod
-    def __valance_l(node: Node) -> Node:
+    def __valance_l(node: Bst) -> Bst:
         if Avl.__f_valance(node.left) == 1:
             node.left = Avl.__valance_r_s(node.left)
 
         return Avl.__valance_l_s(node)
 
     @staticmethod
-    def __valance_r_s(node: Node) -> Node:
+    def __valance_r_s(node: Bst) -> Bst:
         aux = node.right
 
         node.right = aux.left
@@ -265,7 +352,7 @@ class Avl:
         return aux
 
     @staticmethod
-    def __valance_l_s(node: Node) -> Node:
+    def __valance_l_s(node: Bst) -> Bst:
         aux = node.left
 
         node.left = aux.right
@@ -347,7 +434,7 @@ class Dictionary:
     def get(self, k: int | float | Value) -> any:
         node = self.__avl.find(k)
 
-        return Node if node is None else node.value
+        return Bst if node is None else node.value
 
     def set(self, k: int | float | Value, v: any) -> any:
         node = self.__avl.find(k)
@@ -364,17 +451,15 @@ class Dictionary:
         self.set(k, v)
 
 
-class MultiSet(Avl):
+class MultiSet:
     def __init__(self, l: Iterable[int | float | Value] = []):
-        self.__avl = Avl()
+        self.__avl = Avl(MultiBst)
 
         for i in l:
             self.insert(i)
 
     def __iter__(self) -> Iterable[int | float | Value]:
-        for node in self.__avl.in_orden():
-            for i in range(node.value):
-                yield node.key
+        return map(lambda node: node.key, self.__avl.in_orden())
 
     def insert(self, k: int | float | Value):
         node = self.__avl.find(k)
@@ -384,13 +469,12 @@ class MultiSet(Avl):
         else:
             node.value += 1
             node.size += 1
-            self.__avl.update(k)
+            self.__avl.root.update(k)
 
     def remove(self, k: int | float | Value):
         node = self.__avl.find(k)
 
         if node is not None:
-            print(node.size)
             node.value -= 1
             node.size -= 1
 
@@ -400,10 +484,10 @@ class MultiSet(Avl):
             self.__avl.update(k)
 
     def rank(self, k: int | float | Value) -> int:
-        return MultiSet.__rank(self.__avl, k)
+        return self.__avl.rank(k)
 
     def select(self, index: int) -> int | float | Value:
-        node = MultiSet.__select(self.__avl.root, index)
+        node = self.__avl.select(index)
 
         if node is None:
             raise IndexError
@@ -412,38 +496,6 @@ class MultiSet(Avl):
 
     def find(self, k: int | float | Value):
         return self.__avl.find(k) is not None
-
-    def __rank(node: Node | None, k: int | float | Value) -> int:
-        if node is None:
-            return 0
-
-        left = 0 if node.left is None else node.left.size
-
-        if k == node.key:
-            return left
-
-        if k < node.key:
-            return MultiSet._rank(node.left, k)
-
-        return left + node.value + MultiSet.__rank(node.right, k)
-
-    def __select(node: Node | None, index: int) -> Node | None:
-        if node is None:
-            return None
-
-        if index < 0 or index >= node.size:
-            print(node.size)
-            return None
-
-        left = 0 if node.left is None else node.left.size
-
-        if left <= index and index < left + node.value:
-            return node
-
-        if index < left:
-            return MultiSet.__select(node.left, index)
-
-        return MultiSet.__select(node.right, index - left - node.value)
 
     def __getitem__(self, index: int) -> int | float | Value:
         return self.select(index)
--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from DataStructures.disjoin_set import DisjoinSet
-from DataStructures.avl import Avl,Set,Dictionary
+from DataStructures.avl import Avl,Set,Dictionary,MultiSet
 from DataStructures.sorted_algorithms import selection_sort, insertion_sort, merge_sort, quick_sort
 
 # l = [3, 0, 5, -1, 0, 11, 12]
 # quick_sort(l, False)
 # print(l)
 
-a = Set()
+a = MultiSet()
 a.remove(100)
 a.insert(2)
 a.insert(3)
+a.insert(3)
 a.insert(6)
 a.insert(7)
-a.insert(8)
-# a.remove(3)
+# a.insert(8)
+a.remove(3)
+a.remove(3)
 
-a= Dictionary()
+# a= Dictionary()
 
-a.insert(3,2)
-a.insert(5,1)
-a.insert(122,2)
-# a[3]=4
-a[12]=2
+# a.insert(3,2)
+# a.insert(5,1)
+# a.insert(122,2)
+# # a[3]=4
+# a[12]=2
 
 
 for i in a:
     print(i)
 
-print(a.select(1))
-print(a.find(1))
+print(a.rank(7))
+# print(a.find(1))
